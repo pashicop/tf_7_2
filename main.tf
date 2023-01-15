0c59590589d2fcadb0d21a57219da294c8add49d
Pavel shitikov <shitikov@astracom.ru>
2023-01-15 17:53:45 +0300

    network

--- main.tf
+++ main.tf
@@ -30,11 +30,31 @@ resource "yandex_compute_instance" "test_vm" {
   }
 
   network_interface {
-    subnet_id = subnet_id
+    subnet_id = yandex_vpc_subnet.subnet-1.id
     nat       = true
   }
 
   metadata = {
     ssh-keys = "debian10:${file("~/.ssh/id_rsa.pub")}"
   }
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.test_vm.network_interface.0.ip_address
+}
+
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.test_vm.network_interface.0.nat_ip_address
 }
